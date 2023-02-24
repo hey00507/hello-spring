df3e2446a5970f1efbe81524ab117de1a66716ff
EthanKim <hey0507@gmail.com>
2023-02-24 22:53:49 +0900

    [Update] WebApi 관련 개념 정리 및 컨트롤러 생성 (2/24)

--- src/main/java/me/ethan/hellospring/controller/HelloController.java
+++ src/main/java/me/ethan/hellospring/controller/HelloController.java
@@ -1,9 +1,11 @@
 package me.ethan.hellospring.controller;
 
+import jakarta.servlet.annotation.HttpMethodConstraint;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +21,25 @@ public class HelloController {
     public String helloMvc(@RequestParam("name")String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("/hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
