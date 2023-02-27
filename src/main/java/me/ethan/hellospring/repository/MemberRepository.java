d2cbbe93e269fceece83ee4d9d12d2716981e337
EthanKim <hey0507@gmail.com>
2023-02-27 22:34:30 +0900

    [Update] Member 도메인 및 리포지터리 생성 (2/27) - Memory 를 이용한 Repo 생성 - 요구사항 분석하는 방법 등

---
+++ src/main/java/me/ethan/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package me.ethan.hellospring.repository;
+
+
+import me.ethan.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/test/java/me/ethan/hellospring/repository/MemberMemberRepositoryTest.java
@@ -0,0 +1,8 @@
+package me.ethan.hellospring.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberMemberRepositoryTest {
+
+
+}
