3bf76c63af8e34c5a3cf759e8823ac1dbadd4d59
EthanKim <hey0507@gmail.com>
2023-03-02 16:54:44 +0900

    [Feat] CreateMember 기능 개발 (3/2) - MemberForm 객체를 통해, name 을 받아 Member 저장

--- src/main/java/me/ethan/hellospring/controller/MemberController.java
+++ src/main/java/me/ethan/hellospring/controller/MemberController.java
@@ -1,7 +1,10 @@
 package me.ethan.hellospring.controller;
 
+import me.ethan.hellospring.domain.Member;
 import me.ethan.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,17 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String createMember(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
