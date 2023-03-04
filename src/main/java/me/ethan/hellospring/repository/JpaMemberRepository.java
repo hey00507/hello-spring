42a4b2b368b3343ae376d0338bc764f6dc6d6716
EthanKim <hey0507@gmail.com>
2023-03-05 00:32:36 +0900

    [Feat] Spring DATA JPA 적용 - interface 를 상속받아 빠르게 개발하기

--- src/main/java/me/ethan/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/me/ethan/hellospring/repository/JpaMemberRepository.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 @Transactional(readOnly = true)
 public class JpaMemberRepository implements MemberRepository{
 
---
+++ src/main/java/me/ethan/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,13 @@
+package me.ethan.hellospring.repository;
+
+import me.ethan.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
