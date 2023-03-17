ee7448c3501c3087520d904beb1e825b8d5ecf98
EthanKim <hey0507@gmail.com>
2023-03-17 11:27:58 +0900

    [Docs] 다양한 의존관계 주입 (3/17) - 생성자, 메서드, 필드, 수정자 주입

--- src/main/java/me/ethan/hellospring/AutoCoreApplicationConfig.java
+++ src/main/java/me/ethan/hellospring/AutoCoreApplicationConfig.java
@@ -7,12 +7,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoCoreApplicationConfig {
-
     @Bean("memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
